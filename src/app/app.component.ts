7c386e0578416c532609e8001d099b0fc055539a
Arthur <thurdelima@gmail.com>
2024-06-25 01:40:47 -0300

    feat: reafactory of sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver} from '@angular/cdk/layout'
 import { MatSidenav } from '@angular/material/sidenav';
+import { pluck } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +11,39 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class AppComponent {
   title = 'angular-material-simple-admin-reference';
 
-  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  public isSmallScreen = false;
+
+  //@ViewChild(MatSidenav) sidenav!: MatSidenav;
   constructor(private breakPointObserver: BreakpointObserver) {}
 
   ngAfterContentInit(): void {
-    this.breakPointObserver.observe(['(max-width: 800px)']).subscribe({
-      next: (res) => {
-          if(res.matches) {
-            //this.sidenav.mode = 'push';
-            this.sidenav.close()
+    // this.breakPointObserver.observe(['(max-width: 800px)']).subscribe({
+    //   next: (res) => {
+    //       if(res.matches) {
+    //         //this.sidenav.mode = 'push';
+    //         //this.sidenav.close()
+    //         this.isSmallScreen = true;
 
-          } else {
-            ///this.sidenav.mode = 'side';
-            this.sidenav.open()
-          }
-      }
-    })
+    //       } else {
+    //         //this.sidenav.mode = 'side';
+    //         //this.sidenav.open()
+    //         this.isSmallScreen = false;
+    //       }
+    //   }
+    // })
+
+    this.breakPointObserver
+    .observe(['(max-width: 800px)'])
+    .subscribe((res) => this.isSmallScreen = res.matches);
+
+    // this.breakPointObserver.observe(['(max-width: 800px)'])
+    // .pipe(pluck('matches'))
+    // .subscribe({
+    //   next: (res: boolean) => this.isSmallScreen = res;
+    // })
+  }
+
+  get sideNavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
   }
 }
